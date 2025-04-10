63a901cddf17640d41a9b699562378428ce142c3
YZY4070 <muteallchat30@yandex.ru>
2025-04-10 21:03:27 +0300

    Models

---
+++ service/src/main/java/ru/practicum/service/user/User.java
@@ -0,0 +1,27 @@
+package ru.practicum.service.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false, length = 255)
+    String name;
+
+    @Column(nullable = false, unique = true, length = 255)
+    String email;
+
+
+}
